708120ebb3150e08622d92fbb3118788b2e8eb8c
Lautaro Gomez <lautigomez32@gmail.com>
2023-01-03 11:29:54 -0300

    api

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import dotenv
 import sys
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    dotenv.read_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
